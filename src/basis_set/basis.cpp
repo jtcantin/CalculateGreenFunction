6a9e95aaa90c6794b79da7801e6cfde87452ad05
eraserxp <eraser117xp@gmail.com>
2014-11-18 11:36:52 -0800

    add documentation

--- src/basis_set/basis.cpp
+++ src/basis_set/basis.cpp
@@ -11,9 +11,35 @@
  * In a lattice, we give each site an index. For example, in 1D, we can use the
  * index numbers 0 ... xmax to denote each sites. In 2D, we still want to use a
  * single number to denote each lattice site, so we assign a number to each
- * position (x,y). This is a mapping from (x, y) to a positive integer nth.
+ * position (x,y). This is a mapping from (x, y) to a nonnegative integer nth.
  *
- * Given a basis (1D or 2D), this function return the corresponding lattice index.
+ * 1D case (x_for_particle1, x_for_particle1):
+ *
+ * x------x------x------x------...------x------x------x
+ * 0      1      2      3      ...   xmax-2  xmax-1  xmax
+ *
+ *
+ *
+ * 2D case (x_for_p1, y_for_p1; x_for_p2, y_for_p2):
+ *
+ *	 x------x------x------x------...------x------x------x
+ *	 |      |      |      |      ...      |      |      |
+ *	 x------x------x------x------...------x------x------x
+ *	 |      |      |      |      ...      |      |      |
+ *	 x------x------x------x------...------x------x------x
+ *	 |      |      |      |      ...      |      |      |
+ *	 .      .      .      .      ...      .      .      .
+ *	 .      .      .      .      ...      .      .      .
+ *	 .      .      .      .      ...      .      .      .
+ *	 |      |      |      |      ...      |      |      |
+ *	 x------x------x------x------...------x------x------x
+ *(xmax+1)                                          (2*xmax+1)
+ *	 |      |      |      |      ...      |      |      |
+ *	 x------x------x------x------...------x------x------x
+ *	(0)    (1)    (2)            ...                  (xmax)
+ *
+ * Given a two-particle basis (1D or 2D), this function returns the corresponding
+ * lattice indexes for the two sites
  *
  */
 void getLatticeIndex(LatticeShape& lattice, Basis& basis, int &site1, int &site2) {
@@ -31,17 +57,22 @@ void getLatticeIndex(LatticeShape& lattice, Basis& basis, int &site1, int &site2
 		}
 	case 3:
 	default:
-		std::cout << "Not supported!" << std::endl;
+		std::cout << "Dimension >= 3, not supported!" << std::endl;
 	}
 }
 
 
 
-// obtain the ''distance'' between two basis,
-// for example, in 1D the distance between (1,2) and (2,3) is 3-1=2
-// in 2D, the distance between (1,2,3,4) and (1,2,5,6) is sqrt((5-3)^2 + (6-4)^2)
-// if the distance is negative, it means that the two basis doesn't share
-// a common site. In this case, it is meaningless to calculate the distance
+/** This function calculate the ''distance'' between two basis
+ *
+ * For example, in 1D the distance between (1,2) and (2,3) is 3-1=2.
+ * in 2D, the distance between (1,2,3,4) and (1,2,5,6) is sqrt((5-3)^2 + (6-4)^2)
+ *
+ * Note if the two basis don't share a common site, it is meaniningless to
+ * calculate the distance. In this case, the function return -1, indicating
+ * that the function cannot handle the current case.
+ *
+ */
 double distance(Basis& b1, Basis& b2) {
 	// if the two basis are of different dimension, it is meaningless to calculate
 	// the distance
@@ -71,30 +102,83 @@ double distance(Basis& b1, Basis& b2) {
 
 
 /*************************** global variables ***********************/
-//for 1D case: VtoG[K, nth] = G(x1,x2) or basis(x1,x2)
-//for 2D case: VtoG[K, nth] = G(x1, y1, x2, y2) or basis(x1,y1,x2,y2)
-// each element of VtoG is a basis type (an array of size 2 or 4)
-//Basis **VtoG;
+/**
+ * VtoG is a Matrix that is used to convert a vector V to the corresponding
+ * Green's function
+ *
+ * for 1D case: VtoG[K, nth] gives the value of G(x1,x2) that is nth element of V
+ * for 2D case: VtoG[K, nth] gives the value of G(x1, y1, x2, y2) that is nth
+ *              element of V
+ */
 std::vector< std::vector<BasisPointer> > VtoG;
 
-// to store the dimension of V = ( .., G(i-1,j+1), G(i,j), ...)
+// each element of VtoG is a basis type (an array of size 2 or 4)
+//Basis **VtoG;
+
+
+
+
+/**
+ * We group Green's functions into vectors V_K = ( .., G(i-1,j+1), G(i,j), ...)
+ * where K = i + j.
+ *
+ * DimsOfV is used to record the size of various V_K:
+ * size_of_V_K = DimsOfV[K]  (Kmin <= K <= Kmax)
+ */
 std::vector<int> DimsOfV;
 
-//for 1D case: IndexMatrix[x1][x2]=nth  ==>  G(x1,x2) is the nth item in vector V_{x1+x2}
-//for 2D case: IndexMatrix[i][j] = nth with i = x1*(xmax+1) + y1, j = x2*(xmax + 1) + y2
-//              ==> G(x1, y1, x2, y2) is the nth item in vector V_{x1+y1+x2+y2}
+
+/**
+ * IndexMatrix maps a pairs of site index (site1, site2) to an number nth, which
+ * has the following meaning:
+ *
+ * 1D case:
+ *          G(x1, x2) --> 1D basis(x1, x2)
+ *
+ *                    --> (site1=x1, site2=x2) or it can obtained from function
+ *                        getLatticeIndex(lattice, basis, site1, site2)
+ *
+ *                    --> IndexMatrix(site1, site2) = nth
+ *
+ *                    ==> G(x1, x2) is the nth item in the corresponding
+ *                        vector V_{x1+x2}
+ *
+ * 2D case:
+ *          G(x1, y1, x2, y2) --> 2D basis(x1, y1, x2, y2)
+ *
+ *                            --> (site1, site2) can be obtained from function
+ *                                getLatticeIndex(lattice, basis, site1, site2)
+ *
+ *                            --> IndexMatrix(site1, site2) = nth
+ *
+ *                            ==> G(x1, y1, x2, y2) is the nth item in the
+ *                                corresponding vector V_{x1+y1+x2+y2}
+ */
 IMatrix IndexMatrix;
+/***********************************************************************/
 
 
-
+/**
+ * print an array of neighbors for testing purposes
+ */
 void printNeighbors(Neighbors& neighbors) {
 	std::cout << "Print neighbors: " << std::endl;
 	for (int i=0; i<neighbors.size(); ++i) {
 		int dim = neighbors[i].getDim();
+
 		if (dim==1) {
 			std::cout << "x1 = " << neighbors[i][0]
 			          << "\t\t x2 = " << neighbors[i][0] << std::endl;
 		}
+
+		if (dim==2) {
+			// placeholder
+		}
+
+		if (dim==3) {
+			// placeholder
+		}
+
 	}
 }
 
@@ -102,7 +186,7 @@ void printNeighbors(Neighbors& neighbors) {
 
 
 /**
- * Given a two-particle state, this function generate its neighbors that
+ * Given a two-particle basis, this function generates its neighbors that
  * at some distance. The distance can take both positive and negative values.
  * If distance < 0, we call the neighbors left neighbors. If distance > 0,
  * we call the neighbors right neighbors.
--- src/basis_set/basis.h
+++ src/basis_set/basis.h
@@ -18,15 +18,22 @@
 
 #include <boost/shared_ptr.hpp> //include boost library
 
-
+/**
+ * Basis is used to represent the two-particle basis
+ *
+ * 1D:  (x1, x2)
+ * 2D:  (x1, y1; x2, y2)
+ * 3D:  (x1, y1, z1; x2, y2, z2)
+ */
 class Basis {
 public:
+	// default constructor
 	Basis(){
 		dim = 0;
 		coordinates = NULL;
 	}
 
-	// create the basis for 1D cases
+	// create a 1D basis
 	Basis(int x1, int x2) {
 		dim = 1;
 		coordinates = new int[2];
@@ -34,7 +41,7 @@ public:
 		coordinates[1]=x2;
 	}
 
-	// create the basis for 2D cases
+	// create a 2D basis
 	Basis(int x1, int y1, int x2, int y2) {
 		dim = 2;
 		coordinates = new int[4];
@@ -43,6 +50,11 @@ public:
 		coordinates[2]=x2;
 		coordinates[3]=y2;
 	}
+
+	// create a 3D basis
+//	Basis(int x1, int y1, int z1, int x2, int y2, int z2) {
+//
+//	}
 
 	// copy constructor
 	Basis(const Basis& other){
@@ -54,7 +66,7 @@ public:
 
 	// assignment operator
 	Basis& operator= (const Basis& other){
-		// if the basis has already assign space to the array
+		// if the basis has already assigned space for the array
 		if (coordinates!=NULL) {
 			delete [] coordinates;
 		}
@@ -136,41 +148,34 @@ typedef boost::shared_ptr<Basis> BasisPointer;
 typedef std::vector< Basis > Neighbors;
 
 /*************************** global variables ***********************/
-//for 1D case: VtoG[K, nth] = G(x1,x2) or basis(x1,x2)
-//for 2D case: VtoG[K, nth] = G(x1, y1, x2, y2) or basis(x1,y1,x2,y2)
-// each element of VtoG is a basis type (an array of size 2 or 4)
-//extern Basis **VtoG;
-
 extern std::vector< std::vector<BasisPointer> > VtoG;
 
-// to store the dimension of V = ( .., G(i-1,j+1), G(i,j), ...)
 extern std::vector<int> DimsOfV;
 
-//for 1D case: Index[x1][x2]=nth  ==>  G(x1,x2) is the nth item in vector V_{x1+x2}
-//for 2D case: Index[i][j] = nth with i = x1*xmax + y1, j = x2*xmax + y2
-//              ==> G(x1, y1, x2, y2) is the nth item in vector V_{x1+y1+x2+y2}
 extern IMatrix IndexMatrix;
+/*************************** global variables ***********************/
 
 double distance(Basis& b1, Basis& b2);
 
 void printNeighbors(Neighbors& neighbors);
 
-// a class to represent the boundary of a lattice
-// you need to set up the boundary before using it
+
+
+/**
+ * A class that describes a lattice. It includes the information about the
+ * dimension and the boundaries.
+ *
+ * you need to set up the boundaries (xmax, ymax, zmax) before using it
+ * note xmin = ymin = zmin = 0 by default
+ */
 class LatticeShape {
 public:
 
-//	LatticeShape() {
-//		dimension = 0;
-//	}
-
-	// use a vector of size 1, 2, 3 to initialize it
-	// [xmax, ymax, zmax]
-	// note xmin = ymin = zmin = 0
+	// set up the dimension of the lattice
 	LatticeShape(int dim_):dimension(0) {
 		if (dim_!=1 && dim_!=2 && dim_!=3) {
-			std::cout<< "Allowed sizes of the intializer vector are 1, 2, 3"<<std::endl;
-			std::cout<< "The initializer size is " << std::endl;
+			std::cout<< "The input dimension is " << dim_ << std::endl;
+			std::cout<< "Allowed dimensions are 1, 2, 3"<<std::endl;
 			std::exit(-1);
 		}
 
@@ -192,7 +197,7 @@ public:
 
 	int getYmax() {
 		if (dimension<2) {
-			std::cout << "No ymax" << std::endl;
+			std::cout << "Dimension<2, no Ymax" << std::endl;
 			std::exit(-1);
 		}
 		return array[1];
@@ -201,7 +206,7 @@ public:
 
 	int getZmax() {
 		if (dimension<3) {
-			std::cout << "No zmax" << std::endl;
+			std::cout << "Dimension<3, no Zmax" << std::endl;
 			std::exit(-1);
 		}
 		return array[2];
@@ -209,7 +214,7 @@ public:
 
 	void setXmax(int xmax) {
 		if (dimension<1) {
-			std::cout << "No xmax" << std::endl;
+			std::cout << "Dimension<1, no Xmax" << std::endl;
 			std::exit(-1);
 		}
 		array[0] = xmax;
@@ -217,7 +222,7 @@ public:
 
 	void setYmax(int ymax) {
 		if (dimension<2) {
-			std::cout << "No ymax" << std::endl;
+			std::cout << "Dimension<2, no Ymax" << std::endl;
 			std::exit(-1);
 		}
 		array[1] = ymax;
@@ -225,7 +230,7 @@ public:
 
 	void setZmax(int zmax) {
 		if (dimension<3) {
-			std::cout << "No zmax" << std::endl;
+			std::cout << "Dimension<3, no Zmax" << std::endl;
 			std::exit(-1);
 		}
 		array[2] = zmax;
@@ -256,7 +261,6 @@ void getLatticeIndex(LatticeShape& lattice, Basis& basis, int &site1, int &site2
 void generateNeighbors(Basis basis, int distance, LatticeShape& lattice,
         Neighbors& neighbors);
 
-// see the above documentation for VtoG, DimsOfV, and Index
 void generateIndexMatrix(LatticeShape& lattice);
 
 
